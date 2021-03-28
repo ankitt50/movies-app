554b3346dfea7fad619afc99fa96b36f3a962f05
Ankit Tripathi <ankit.tripathi@cybergindia.local>
2021-03-28 22:40:37 +0530

    Added username and password inside Login form

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -2,47 +2,16 @@ import React,{Component} from 'react';
 import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assests/logo.svg';
-import Modal from 'react-modal';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 
 class Header extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            isModalOpen: false,
-            value: 0
-        };
-    }
-    openModalHandler = () => {
-        this.setState({isModalOpen: true,value: 0});
-    }
-
-    closeModalHandler = () => {
-        this.setState({isModalOpen: false, value: 0});
-    }
-    tabChangeHandler = (event, value) => {
-        this.setState({isModalOpen: true, value: value});
-    }
-
+    
     render() {
-        return <div>
-<div className="header-outer-div">
+        return <div className="header-outer-div">
             <div className="header-inner-icon-div">
                 <img className="app-logo" src={logo} alt="movies-app-logo-icon"/>
             </div>
-            <Button id="login-btn" variant="contained" onClick={this.openModalHandler}>Login</Button>
+            <Button id="login-btn" variant="contained" onClick={this.props.openModalHandler}>Login</Button>
         </div>
-        <Modal ariaHideApp={false} isOpen={this.state.isModalOpen} contentLabel="Login" onRequestClose={this.closeModalHandler}>
-        <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
-            <Tab label="Login"/>
-            <Tab label="Register"/>
-        </Tabs>
-        </Modal>
-        </div>
-        
-        
     }
 }
 export default Header;
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,10 +1,79 @@
 import React,{Component} from 'react';
 import './Home.css';
 import Header from '../../common/header/Header';
+import Modal from 'react-modal';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+
+
+function TabContainer(props) {
+    return (<Typography component="div" style={{padding:0}}>
+        {props.children}
+    </Typography>);
+}
 
 class Home extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            isModalOpen: false,
+            value: 0
+        };
+    }
+
+    openModalHandler = () => {
+        this.setState({isModalOpen: true,value: 0});
+    }
+
+    closeModalHandler = () => {
+        this.setState({isModalOpen: false, value: 0});
+    }
+    tabChangeHandler = (event, value) => {
+        this.setState({isModalOpen: true, value: value});
+    }
+
+
     render() {
-        return <Header/>;
+
+        const customStyles = {
+            content: {
+                top: '50%',
+                left: '50%',
+                right: 'auto',
+                bottom: 'auto',
+                marginRight: '-50%',
+                transform: 'translate(-50%, -50%)',
+            }
+        };
+
+        return <div className="main-page-outermost-div">
+            <Header openModalHandler={this.openModalHandler}/>;
+            <Modal id="login-register-modal" ariaHideApp={false} isOpen={this.state.isModalOpen} contentLabel="Login" onRequestClose={this.closeModalHandler} style={customStyles}>
+        <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+            <Tab label="Login"/>
+            <Tab label="Register"/>
+        </Tabs>
+        <TabContainer>
+            <div className="form-control-container">
+            <FormControl className="form-control-login" required>
+                <InputLabel htmlFor="userName">Username</InputLabel>
+                <Input id="userName" type="text"/>
+            </FormControl>
+            </div>
+           <div className="form-control-container">
+           <FormControl className="form-control-login" required>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input id="password" type="password"/>
+            </FormControl>
+           </div>
+        </TabContainer>
+        </Modal>
+        </div>
     }
 }
 
